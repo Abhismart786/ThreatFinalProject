182c1bab0e8abab6396a2e23804e80608e5e21e4
Abhismart786 <abhiKhatri097@gmail.com>
2024-12-06 13:07:13 -0500

    sixth commit

--- Threat App/Authentication/RegistrationView.swift
+++ Threat App/Authentication/RegistrationView.swift
@@ -8,8 +8,73 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    @State private var email = ""
+    @State private var password = ""
+    @State private var fullname = ""
+    @State private var username = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Spacer()
+            Image("threads")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 120,height: 120)
+                .padding()
+            VStack {
+                TextField("Enter your email", text : $email)
+                    .font(.subheadline)
+                    .padding(12)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+                    .padding(.horizontal,24)
+                
+                SecureField("Enter your password", text: $password)
+                    .font(.subheadline)
+                    .padding(12)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+                    .padding(.horizontal,24)
+                
+                TextField("Enter your fullname", text : $fullname)
+                    .font(.subheadline)
+                    .padding(12)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+                    .padding(.horizontal,24)
+                
+                TextField("Enter your username", text : $username)
+                    .font(.subheadline)
+                    .padding(12)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+                    .padding(.horizontal,24)
+            }
+            Button{
+                
+            }label: {
+                Text("Sign up")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .frame(width: 332,height: 44)
+                    .background(.black)
+                    .cornerRadius(8)
+            }
+            .padding(.vertical)
+            Spacer()
+            Divider()
+            Button{
+                
+            }label: {
+                HStack{
+                    Text("Already have an account? ")
+                    Text("Sign Up")
+                        .fontWeight(.black)
+                }
+                .foregroundColor(.black)
+                .font(.footnote)
+            }
+        }
     }
 }
 
