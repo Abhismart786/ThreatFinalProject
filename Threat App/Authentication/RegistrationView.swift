45a5ba9966ffc20637d7fab0cb43d3784b0612c5
Abhismart786 <abhiKhatri097@gmail.com>
2024-12-06 12:51:15 -0500

    fifth commit

---
+++ Threat App/Authentication/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  Threat App
+//
+//  Created by Abhishek Sehgal on 2024-12-06.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
