4daa17ead3a0f7aeb9877bc14d83f9ef982a8b2a
Abhismart786 <abhiKhatri097@gmail.com>
2024-12-06 10:18:08 -0500

    first commit

---
+++ Threat App/Threat_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Threat_AppApp.swift
+//  Threat App
+//
+//  Created by Abhishek Sehgal on 2024-12-06.
+//
+
+import SwiftUI
+
+@main
+struct Threat_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Threat AppTests/Threat_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Threat_AppTests.swift
+//  Threat AppTests
+//
+//  Created by Abhishek Sehgal on 2024-12-06.
+//
+
+import Testing
+@testable import Threat_App
+
+struct Threat_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
