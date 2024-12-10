eea21238028fcb354e6ef636471bc7b20a33009a
Abhismart786 <abhiKhatri097@gmail.com>
2024-12-10 10:20:30 -0500

    26 commit

--- Threat App/Views/ExploreView.swift
+++ Threat App/Views/ExploreView.swift
@@ -16,28 +16,33 @@ struct ExploreView: View {
                     ForEach(0 ... 10, id: \.self)
                     {
                         user in
-                        HStack{
-                            Image(systemName: "person.circle")
-                                .scaleEffect(2)
-                                .frame(width: 30,height:40)
-                                .imageScale(.large)
-                                .clipShape(Circle())
-                            VStack(alignment: .leading)
-                            {
-                                Text("Abhishek")
+                        VStack {
+                            HStack{
+                                Image(systemName: "person.circle")
+                                    .scaleEffect(2)
+                                    .frame(width: 30,height:40)
+                                    .imageScale(.large)
+                                    .clipShape(Circle())
+                                VStack(alignment: .leading)
+                                {
+                                    Text("Abhishek")
+                                        .fontWeight(.semibold)
+                                    
+                                    Text("Max Verstappen")
+                                }
+                                .font(.footnote)
+                                Spacer()
+                                Text("Follow")
+                                    .font(.subheadline)
                                     .fontWeight(.semibold)
-                                
-                                Text("Max Verstappen")
+                                    .frame(width: 100,height: 32)
                             }
-                            .font(.footnote)
-                            Spacer()
-                            Text("Follow")
-                                .font(.subheadline)
-                                .fontWeight(.semibold)
-                                .frame(width: 100,height: 32)
                         }
+                        .padding(.horizontal)
+                        Divider()
                     }
-                    .padding(.horizontal)
+                    .padding(.vertical,4)
+                    
                 }
             }
             .navigationTitle("Search")
