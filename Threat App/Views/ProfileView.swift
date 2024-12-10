b443eb40cbb77a57a6d04a2cdb89f10fc006e317
Abhismart786 <abhiKhatri097@gmail.com>
2024-12-10 10:34:08 -0500

    27 commit

--- Threat App/Views/ProfileView.swift
+++ Threat App/Views/ProfileView.swift
@@ -9,7 +9,49 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text("Profile View")
+        ScrollView(showsIndicators: false){
+            VStack(spacing: 20){
+                HStack(alignment: .top)
+                {
+                    VStack(alignment: .leading, spacing: 12)
+                    {
+                        VStack(alignment: .leading, spacing: 4)
+                        {
+                            Text("Abhishek Sehgal")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                            
+                            Text("abhishek_sehgal")
+                                .font(.subheadline)
+                        }
+                        Text("Never Give up!")
+                            .font(.footnote)
+                        Text("10 followers")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                    }
+                    Spacer()
+                    Image(systemName: "person.circle")
+                        .scaleEffect(2)
+                        .frame(width: 30,height:40)
+                        .imageScale(.large)
+                        .clipShape(Circle())
+                }
+                Button{
+                    
+                }label: {
+                    Text("Follow")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(width: 352,height: 32)
+                        .background(.black)
+                        .cornerRadius(8)
+                }
+            }
+            
+        }
+        .padding(.horizontal)
     }
 }
 
