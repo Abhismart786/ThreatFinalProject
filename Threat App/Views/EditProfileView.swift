3a49d379a20ce184da3cc3a9d2f50e008f84aeb4
Abhismart786 <abhiKhatri097@gmail.com>
2024-12-27 15:03:33 -0500

    29 commit

--- Threat App/Views/EditProfileView.swift
+++ Threat App/Views/EditProfileView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct EditProfileView: View {
+    @State private var bio = ""
+    @State private var link = ""
+    @State private var isPrivateProfile = false
     var body: some View {
         NavigationStack{
             ZStack{
                 Color(.systemGroupedBackground)
                     .edgesIgnoringSafeArea([.bottom,.horizontal])
                 VStack{
-                    
+                    HStack{
+                        VStack(alignment:.leading){
+                            Text("Name")
+                                .fontWeight(.semibold)
+                            Text("Abhishek Sehgal")
+                        }
+                        .font(.footnote)
+                        Spacer()
+                        Image(systemName: "person.circle")
+                            .scaleEffect(2)
+                            .frame(width: 30,height:30)
+                            .imageScale(.large)
+                            .clipShape(Circle())
+                    }
+                    Divider()
+                    VStack(alignment:.leading){
+                        Text("Bio")
+                            .fontWeight(.semibold)
+                        TextField("Enter your bio..",text: $bio,axis: .vertical)
+                    }
+                    .font(.footnote)
+                    Divider()
+                    VStack(alignment:.leading){
+                        Text("Link")
+                            .fontWeight(.semibold)
+                        TextField("Enter your Link..",text: $link)
+                    }
+                    .font(.footnote)
+                    Divider()
+                    Toggle("Private profile", isOn: $isPrivateProfile)
                 }
+                .font(.footnote)
+                .padding()
+                .background(.white)
+                .cornerRadius(10)
+                .padding()
             }
             .navigationTitle("Edit Profile")
             .navigationBarTitleDisplayMode(.inline)
