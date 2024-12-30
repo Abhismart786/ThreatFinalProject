a5a4aa081b0df60823a6d850160a80182d99287b
Abhismart786 <abhiKhatri097@gmail.com>
2024-12-29 21:49:20 -0500

    30 commit

--- Threat App/Views/CreateThreadView.swift
+++ Threat App/Views/CreateThreadView.swift
@@ -8,8 +8,57 @@
 import SwiftUI
 
 struct CreateThreadView: View {
+    @State private var caption = ""
     var body: some View {
-        Text("Thread View")
+        NavigationStack{
+            VStack{
+                HStack(alignment:.top){
+                    Image(systemName: "person.circle")
+                        .scaleEffect(2)
+                        .frame(width: 30,height:30)
+                        .imageScale(.large)
+                        .clipShape(Circle())
+                    VStack(alignment:.leading,spacing: 4)
+                    {
+                        Text("Hello World")
+                            .fontWeight(.semibold)
+                        TextField("Start a thread...",text:   $caption,axis: .vertical)
+                    }.font(.footnote)
+                    Spacer()
+                    if !caption.isEmpty{
+                        Button{
+                            caption = ""
+                        }label: {
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 12,height: 12)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
+                Spacer()
+                
+            }.padding()
+            .navigationTitle("New Thread")
+                .navigationBarTitleDisplayMode(.inline)
+            .toolbar{
+                ToolbarItem(placement:.navigationBarLeading){
+                    Button("Cancel"){
+                        
+                    }.font(.subheadline)
+                        .foregroundColor(.black)
+                }
+                ToolbarItem(placement:.navigationBarTrailing)
+                {
+                    Button("Post"){
+                        
+                    } .font(.subheadline)
+                      .fontWeight(.semibold)
+                      .foregroundColor(.black)
+                }
+                
+            }
+        }
     }
 }
 
