4a978f47fe238dce1565e6f9579a323440bd9c4e
Abhismart786 <abhiKhatri097@gmail.com>
2025-01-02 13:44:40 -0500

    40 commit

--- Threat App/Authentication/AuthService.swift
+++ Threat App/Authentication/AuthService.swift
@@ -6,6 +6,7 @@
 //
 
 import FirebaseAuth
+import FirebaseFirestore
 class AuthService{
     
     @Published var userSession: FirebaseAuth.User?  // used to check the user loged in or not
@@ -18,7 +19,7 @@ class AuthService{
         do{
             let result = try await Auth.auth().signIn(withEmail: email, password: password)
             self.userSession = result.user
-            print("DEBUG: Created user \(result.user.uid)")
+           
         }catch{
             print("DEBUG Failed to create user\(error.localizedDescription)")
         }
@@ -29,6 +30,7 @@ class AuthService{
             let result = try await Auth.auth().createUser(withEmail: email, password: password)
             self.userSession = result.user
             print("DEBUG: Created user \(result.user.uid)")
+            try await uploadUserData(withEmail: email, fullname: fullname, username:username, id: result.user.uid)
         }catch{
             print("DEBUG Failed to create user\(error.localizedDescription)")
         }
@@ -36,5 +38,15 @@ class AuthService{
     func signOut(){
         try? Auth.auth().signOut() // signout on backend
         self.userSession = nil //removal of user session
+    }
+    @MainActor
+    private func uploadUserData(withEmail email:String,fullname: String,username:String,id:String)async throws
+    {
+        let user = User(id: id, fullname: fullname, email: email, username: username)
+        guard let userData = try? Firestore.Encoder().encode(user)
+        else{
+            return
+        }
+        try await Firestore.firestore().collection("users").document(id).setData(userData)
     }
 }
--- Threat App/Views/User.swift
+++ Threat App/Views/User.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+struct User: Identifiable,Codable{
+    let id: String
+    let fullname: String
+    let email: String
+    let username: String
+    var bio: String?
+    var profileImage: String?
+}
